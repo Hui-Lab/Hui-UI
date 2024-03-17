d7004da37e51d9792043dddcb7dccd23aa172edb
hoangquocvietuet <hoangquocvietuet@gmail.com>
2024-03-17 07:22:08 +0700

    updated Loan

--- src/assets/Component/Loan/Loan.jsx
+++ src/assets/Component/Loan/Loan.jsx
@@ -5,51 +5,53 @@ import { ChainContext } from "../../../Context/ChainContext";
 const LoanLineaTestnet = (explorerURL) => {
 	explorerURL = explorerURL.explorerURL;
 	return (
-		<table class="styled-table">
-			<thead>
-				<tr>
-					<th>Name</th>
-					<th>Address</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<td>HUI Token</td>
-					<td>
-						<a
-							href={
-								explorerURL +
-								`address/0xaCb1770f375f07bC00DfdbfE12daB0e5CC784C41`
-							}
-						>
-							0xaCb1770f375f07bC00DfdbfE12daB0e5CC784C41
-						</a>
-					</td>
-				</tr>
-				<tr>
-					<td>HUI Provider</td>
-					<td>
-						<a
-							href={
-								explorerURL +
-								`address/0xB253170687506419789eE0d8b01DdbdEEb13c5d3`
-							}
-						>
-							0xB253170687506419789eE0d8b01DdbdEEb13c5d3
-						</a>
-					</td>
-				</tr>
-				<tr>
-					<td>HUI Protocol Github</td>
-					<td>
-						{" "}
-						<a href={"https://github.com/orgs/Hui-Lab/repositories"}>
-							https://github.com/orgs/Hui-Lab/repositories
-						</a>
-					</td>
-				</tr>
-			</tbody>
-		</table>
+		<div>
+			<table class="styled-table">
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Address</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>HUI Token</td>
+						<td>
+							<a
+								href={
+									explorerURL +
+									`address/0xaCb1770f375f07bC00DfdbfE12daB0e5CC784C41`
+								}
+							>
+								0xaCb1770f375f07bC00DfdbfE12daB0e5CC784C41
+							</a>
+						</td>
+					</tr>
+					<tr>
+						<td>HUI Provider</td>
+						<td>
+							<a
+								href={
+									explorerURL +
+									`address/0xB253170687506419789eE0d8b01DdbdEEb13c5d3`
+								}
+							>
+								0xB253170687506419789eE0d8b01DdbdEEb13c5d3
+							</a>
+						</td>
+					</tr>
+					<tr>
+						<td>HUI Protocol Github</td>
+						<td>
+							{" "}
+							<a href={"https://github.com/orgs/Hui-Lab/repositories"}>
+								https://github.com/orgs/Hui-Lab/repositories
+							</a>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
 	);
 };
 
@@ -114,6 +116,9 @@ export default function Loan() {
 			) : (
 				<LoanPolygonZKEVMTestnet explorerURL={activeChain.explorerURL} />
 			)}
+			<div>
+				<h1>Available loan</h1>
+			</div>
 		</div>
 	);
 }
